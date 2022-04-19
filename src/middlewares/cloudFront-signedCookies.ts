239f88f0d62c6de9bd99c78622ac3b392e9802e5
RW <rwu13795@gmail.com>
2022-04-19 17:14:43 -0400

    adding signed-cookies

--- src/middlewares/cloudFront-signedCookies.ts
+++ src/middlewares/cloudFront-signedCookies.ts
@@ -6,24 +6,27 @@ export const cloudFront_signedCookies = async (
   res: Response,
   next: NextFunction
 ) => {
+  // this middleware will set a 1 hour signed cookies for both the private folder
+  // and group folder whenever a auth-user send a request to the server.
   const currentUser = req.session.currentUser;
-
-  console.log(req.headers.cookie);
 
   if (!currentUser || !currentUser.isLoggedIn) {
     return next();
   }
 
-  const folder_user = `${process.env.CLOUD_FRONT_URL}/users/${currentUser.user_id}`;
+  const folder_user = `${process.env.CLOUD_FRONT_URL}/users/${currentUser.user_id}/*`;
+  const folder_groups = `${process.env.CLOUD_FRONT_URL}/groups/*`;
 
   const policy_user = createPolicy(folder_user);
+  const policy_groups = createPolicy(folder_groups);
 
   const cookie_user = cloudFront_signer.getSignedCookie({
     policy: policy_user,
   });
-  // const cookie_test2 = cloudFront_signer.getSignedCookie({
-  //   policy: policy_2,
-  // });
+
+  const cookie_groups = cloudFront_signer.getSignedCookie({
+    policy: policy_groups,
+  });
 
   // only set the "CloudFront-Key-Pair-Id" once since, it is the same for the all cookies
   res.cookie("CloudFront-Key-Pair-Id", cookie_user["CloudFront-Key-Pair-Id"], {
@@ -33,34 +36,42 @@ export const cloudFront_signedCookies = async (
     path: "/",
   });
 
-  // set Policy and Signature for the 2 differnt folders
+  // user
   res.cookie("CloudFront-Policy", cookie_user["CloudFront-Policy"], {
     domain: "reachat.live",
     httpOnly: true,
     path: "/users",
   });
-  // the corresponding path of which the img source in the client contains
   res.cookie("CloudFront-Signature", cookie_user["CloudFront-Signature"], {
     domain: "reachat.live",
     httpOnly: true,
     path: "/users",
   });
-  // res.cookie("CloudFront-Policy", cookie_test2["CloudFront-Policy"], {
-  //   domain: "reachat.live",
-  //   httpOnly: true,
-  //   path: "/testing-2",
-  // });
-  // res.cookie("CloudFront-Signature", cookie_test2["CloudFront-Signature"], {
-  //   domain: "reachat.live",
-  //   httpOnly: true,
-  //   path: "/testing-2",
-  // });
 
-  console.log("res.cookie", res.cookie);
+  // groups
+  res.cookie("CloudFront-Policy", cookie_groups["CloudFront-Policy"], {
+    domain: "reachat.live",
+    httpOnly: true,
+    path: "/groups",
+  });
+  res.cookie("CloudFront-Signature", cookie_groups["CloudFront-Signature"], {
+    domain: "reachat.live",
+    httpOnly: true,
+    path: "/groups",
+  });
 
   next();
 };
 
+// NOTE //
+// There can be only ONE statement in each policy for each resource path
+// I have to set a cookie for each one of the policies
+// the worst part is that the size of this pair of cookies is large, at most
+// 5 policy-cookies can be set at the same time.
+
+// since I could only set 5 signed-cookies, I could not set cookies for all
+// the specific group folders. (Maybe I could use aws-lambda function to restrict the
+// access for each individual group?)
 function createPolicy(resourceFolder: string) {
   return JSON.stringify({
     Statement: [
